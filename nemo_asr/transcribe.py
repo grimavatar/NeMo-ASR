50ad13f3bcec6dec59f2a273682e6504ba264364
grimavatar <57611284+grimavatar@users.noreply.github.com>
2026-04-17 09:42:24 -0400

    Update transcribe.py

--- nemo_asr/transcribe.py
+++ nemo_asr/transcribe.py
@@ -15,8 +15,9 @@ from .utils.resample import resample
 
 
 class NeMoASR:
-    def __init__(self, model_name: str = "nvidia/parakeet-tdt-0.6b-v3", target_sr: int = 16000) -> tuple[list[str], list[list[dict]]]:
+    def __init__(self, model_name: str = "nvidia/parakeet-tdt-0.6b-v3", sr: int = 16000) -> tuple[list[str], list[list[dict]]]:
         self.model = nemo_asr.models.ASRModel.from_pretrained(model_name = model_name)
+        self.sr = int(sr)
     
     def transcribe(self, audio: str | Path | tuple[np.ndarray, int] | list, alignment_level: str = "word"):
         alignment_level = alignment_level.lower()
@@ -29,20 +30,20 @@ class NeMoASR:
         if not isinstance(audio, list):
             audio = [audio]
         
-        audio = [self.load_audio(e, target_sr) for e in audio]
+        audio = [self.load_audio(e) for e in audio]
 
         results = self.model.transcribe(audio, timestamps=True, use_lhotse=False, verbose=False)
         texts = [e.text.strip() for e in results]
         alignments = [e.timestamp[alignment_level] for e in results]
         return texts, alignments
     
-    def load_audio(self, audio: str | Path | tuple[np.ndarray, int], target_sr: int) -> np.ndarray:
+    def load_audio(self, audio: str | Path | tuple[np.ndarray, int]) -> np.ndarray:
         if isinstance(audio, (str, Path)):
             wav, sr = sf.read(audio, dtype = "float32", always_2d = False)
         else:
             wav, sr = audio
             if not (isinstance(wav, np.ndarray) and isinstance(sr, int)):
                 raise ValueError(f"'audio' must be a str, Path, or tuple of (np.ndarray, int), but got ({type(wav).__name__}, {type(sr).__name__})")
-        if sr != target_sr:
-            wav = resample(y = wav, orig_sr = int(sr), target_sr = target_sr)
+        if sr != self.sr:
+            wav = resample(y = wav, orig_sr = int(sr), target_sr = self.sr)
         return wav
