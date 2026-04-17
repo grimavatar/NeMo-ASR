5414f25dc05799398a90d8329b64d15d84938fd1
grimavatar <57611284+grimavatar@users.noreply.github.com>
2026-04-17 09:32:27 -0400

    Update transcribe.py

--- nemo_asr/transcribe.py
+++ nemo_asr/transcribe.py
@@ -15,7 +15,7 @@ from .utils.resample import resample
 
 
 class NeMoASR:
-    def __init__(self, model_name: str = "nvidia/parakeet-tdt-0.6b-v3"):
+    def __init__(self, model_name: str = "nvidia/parakeet-tdt-0.6b-v3", target_sr: int = 16000) -> tuple[list[str], list[list[dict]]]:
         self.model = nemo_asr.models.ASRModel.from_pretrained(model_name = model_name)
     
     def transcribe(self, audio: str | Path | tuple[np.ndarray, int] | list, alignment_level: str = "word"):
@@ -29,14 +29,14 @@ class NeMoASR:
         if not isinstance(audio, list):
             audio = [audio]
         
-        audio = [self.load_audio(e) for e in audio]
+        audio = [self.load_audio(e, target_sr) for e in audio]
 
         results = self.model.transcribe(audio, timestamps=True, use_lhotse=False, verbose=False)
         texts = [e.text.strip() for e in results]
         alignments = [e.timestamp[alignment_level] for e in results]
         return texts, alignments
     
-    def load_audio(self, audio: str | Path | tuple[np.ndarray, int], target_sr: int = 16000):
+    def load_audio(self, audio: str | Path | tuple[np.ndarray, int], target_sr: int) -> np.ndarray:
         if isinstance(audio, (str, Path)):
             wav, sr = sf.read(audio, dtype = "float32", always_2d = False)
         else:
